f1f9d47bc45a29bfe2185d344cdc94a242c8c17a
gavin stahl <stahlgavin@gmail.com>
2020-01-22 18:29:49 -0800

    fixed css issue with the box

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -7,7 +7,7 @@ const AddedFeature = props => {
     <li>
       {/* Add an onClick to run a function to remove a feature */}
       <button
-        className="button"
+        className="button removeSpace"
         onClick={() => props.removeFeature(props.feature)}
       >
         X
